1bf85fd35f00fc6d1872fd202dd2f9baa2f69080
Galande Rohit Khandu <80148975+rohitgalande1032@users.noreply.github.com>
2022-11-12 17:35:02 +0530

    first commit

---
+++ db/conn.js
@@ -0,0 +1,19 @@
+   
+// database connections
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+const uri = 'mongodb+srv://jwtuser:pass%4011@cluster0.vzhly.mongodb.net/RohitFileStore';
+// const uri ='mongodb://localhost:27017/amazon-products' ;
+
+mongoose.connect(uri, {
+    useNewUrlParser: true, useUnifiedTopology: true
+}).then((data) => {
+    console.log("DB is connected..");
+}).catch((err) => {
+    console.log(err);
+});
+const connection = mongoose.connection;
+connection.once("open", () => {
+    console.log("Database connected sucessfully");
+})
